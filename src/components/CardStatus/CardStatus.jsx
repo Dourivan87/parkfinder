24102c54407bff3e13eaae95bbf6c6dc106c1a16
Visitante Digital <visitante.digital@DIGITALCOLLEGE.LOCAL>
2024-10-14 21:57:31 -0300

    added pages

---
+++ src/components/CardStatus/CardStatus.jsx
@@ -0,0 +1,17 @@
+export default function CarStatus ()  {
+    return {
+   div className ="h-36> w-56 bg-blue-950 rounded-2xl flex flex-co text-white">
+     <div className ="flex min-w-full">
+       <div className="flex-1 flex justify-center ">
+           1
+      </div>
+      <div className="flex-1 flex justify-center">
+          2
+     </div>
+   <div className="flex justify-center">
+         FOOTER
+     </div>
+   </div>
+  </div>
+    
+}
--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,7 +1,25 @@
+"use client"; 
+import{ Car,LetterCircleP  } from "@phosphor-icons/react/dist/ssr"
+
+import { usePathname,useRouter } from "next/navigation";
 export default function SideBar(){
+      const pathname = usePathname()
+      const router = useRouter()
+
+      function goToPage(page) {
+            router.push(page)
+      }
+
     return (
-        <div className="absolute left-0 min-h-screen min-w-24 p-2 flex justify-center bg-blue-950 text-white">
-                A
+        <div className="absolute left-0 min-h-screen min-w-24 p-2 flex  
+        justify-start items-center bg-blue-950 text-white flex-col space-y-2 ">
+                
+                <div onClick={() => goToPage ("dashboard")} className={pathname === "/dashboard" ? "bg-green-600 p-2 rounded-md cursor-pointer":"cursor-pointer"}>
+                    <Car color="White" size={26} />
+                </div>
+                <div onClick={() => goToPage ("park")} className={pathname === "/park" ? "bg-green-600 p-2 rounded-md cursor-pointer":"cursor-pointer"}>
+                    <LetterCircleP color="White" size={26} />
+                </div>
         </div>
     )
-}
+} 
